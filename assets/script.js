fc0b687cb92b62f9b69f737c81cd8d8503b88ef2
Avinash <avinashanish684@gmail.com>
2024-08-25 18:01:18 +0530

    second commit

--- assets/script.js
+++ assets/script.js
@@ -21,3 +21,11 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
